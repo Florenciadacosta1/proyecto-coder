4c4da6ff6797a2a883a095c3d703c8b4cbe576a8
Florencia Da Costa <florenciadacosta117@gmail.com>
2022-10-25 23:20:11 -0300

    Agregado script con ciclo y condicional

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let seleccionado = prompt("¿Desea agregar al carrito?");
+
+if( seleccionado = si ) {
+    alert("Articulo ingresado")
+} else {
+    alert("Articulo no ingresado")
+}
+
+let fondo = 0;
+
+let calza = 9000;
+
+pagos = pagos + calza;
+
+let top = 4500;
+
+pagos = pagos + top;
+
+let buzo = 1300;
+
+pagos = pagos + buzo;
+
+console.log("a pagar: " + pagos);
